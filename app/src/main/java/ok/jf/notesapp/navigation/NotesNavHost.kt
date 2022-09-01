42eea9a3451a470fc50d7ccecdc190adec9fed62
Developer <jen000@yandex.ru>
2022-09-01 11:59:46 +0300

    New add button and inputs

--- app/src/main/java/ok/jf/notesapp/navigation/NotesNavHost.kt
+++ app/src/main/java/ok/jf/notesapp/navigation/NotesNavHost.kt
@@ -4,16 +4,13 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import ok.jf.notesapp.screens.Add
-import ok.jf.notesapp.screens.Main
-import ok.jf.notesapp.screens.Note
-import ok.jf.notesapp.screens.Start
+import ok.jf.notesapp.screens.*
 
-sealed class NavRoute(val route: String){
-    object Start: NavRoute("start_screen")
-    object Main: NavRoute("main_screen")
-    object Add: NavRoute("add_screen")
-    object Note: NavRoute("note_screen")
+sealed class NavRoute(val route: String) {
+    object Start : NavRoute("start_screen")
+    object Main : NavRoute("main_screen")
+    object Add : NavRoute("add_screen")
+    object Note : NavRoute("note_screen")
 
 }
 
@@ -21,11 +18,11 @@ sealed class NavRoute(val route: String){
 fun NotesNavHost() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = NavRoute.Start.route){
-        composable(NavRoute.Start.route) { Start(navController = navController) }
-        composable(NavRoute.Main.route) { Main(navController = navController) }
-        composable(NavRoute.Add.route) { Add(navController = navController) }
-        composable(NavRoute.Note.route) { Note(navController = navController) }
+    NavHost(navController = navController, startDestination = NavRoute.Start.route) {
+        composable(NavRoute.Start.route) { StartScreen(navController = navController) }
+        composable(NavRoute.Main.route) { MainScreen(navController = navController) }
+        composable(NavRoute.Add.route) { AddScreen(navController = navController) }
+        composable(NavRoute.Note.route) { NoteScreen(navController = navController) }
 
     }
 }
--- app/src/main/java/ok/jf/notesapp/screens/Main.kt
+++ app/src/main/java/ok/jf/notesapp/screens/Main.kt
@@ -1,8 +1,79 @@
 package ok.jf.notesapp.screens
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import ok.jf.notesapp.navigation.NavRoute
+import ok.jf.notesapp.ui.theme.NotesAPPTheme
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 
 @Composable
-fun Main(navController: NavHostController){
+fun MainScreen(navController: NavHostController) {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                navController.navigate(NavRoute.Add.route)
+            }) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = "Add",
+                    tint = Color.White
+                )
+            }
+        }
+    ) {
+        Column() {
+            NoteItem(title = "Note1", subtitles = "Subtitles for note 1", navController = navController)
+            NoteItem(title = "Note2", subtitles = "Subtitles for note 2", navController = navController)
+            NoteItem(title = "Note3", subtitles = "Subtitles for note 3", navController = navController)
+        }
+    }
+}
 
+@Composable
+fun NoteItem(title: String, subtitles: String, navController: NavHostController){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp, horizontal = 24.dp)
+            .clickable {
+                navController.navigate(NavRoute.Note.route)
+            },
+        elevation = 6.dp
+    ) {
+        Column(modifier = Modifier.padding(vertical = 8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ) {
+            Text(
+                text = title,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(text = subtitles)
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun prevMainScreen() {
+    NotesAPPTheme {
+        MainScreen(navController = rememberNavController())
+    }
 }
